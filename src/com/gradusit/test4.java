2d5e87f352e1ea0135b5e732de02f34bde8be274
Gustavo Folga <gfolga@salt.com>
2014-10-10 10:49:37 -0200

    if example

---
+++ src/com/gradusit/test4.java
@@ -0,0 +1,20 @@
+package com.gradusit;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Gustavo
+ * Date: 10/10/14
+ * Time: 10:41
+ * To change this template use File | Settings | File Templates.
+ */
+public class test4 {
+    public static void main(String args[]) {
+        int a;
+        int b;
+        a = 1;
+        b = 2;
+        if ( a > b){ System.out.println("a mayor a b");}
+        if ( b > a){ System.out.println("b mayor a a");}
+
+    }
+}
