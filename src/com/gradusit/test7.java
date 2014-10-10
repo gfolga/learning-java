ee2a47429d1791aba7dbb6f0e75c426e3fc45d25
Gustavo Folga <gfolga@salt.com>
2014-10-10 11:56:37 -0200

    if else

---
+++ src/com/gradusit/test7.java
@@ -0,0 +1,25 @@
+package com.gradusit;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Gustavo
+ * Date: 10/10/14
+ * Time: 11:52
+ * To change this template use File | Settings | File Templates.
+ */
+public class test7 {
+    public static void main(String args[]){
+        int A, B ;
+        A = 10;
+        B = 20;
+        if( A < B )
+        {
+            System.out.println("A menor a B");
+        }
+        else
+        {
+            System.out.println("A mayor a B");
+        }
+
+    }
+}
