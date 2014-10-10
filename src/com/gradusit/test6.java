508c86d730d928446e406bf9439c0205ffdc1605
Gustavo Folga <gfolga@salt.com>
2014-10-10 11:51:17 -0200

    in.read example

---
+++ src/com/gradusit/test6.java
@@ -0,0 +1,18 @@
+package com.gradusit;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Gustavo
+ * Date: 10/10/14
+ * Time: 11:43
+ * To change this template use File | Settings | File Templates.
+ */
+public class test6 {
+    public static void main(String args[])
+        throws java.io.IOException {
+        char C;
+        C = (char) System.in.read();
+        System.out.println("Leyendo del keyboard: " + C);
+    }
+}
+
