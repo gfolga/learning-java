191c3afffb2fbfa2e9bdb2f6a40d861c45f2a2a9
Gustavo Folga <gfolga@salt.com>
2014-10-10 10:55:29 -0200

    for example

---
+++ src/com/gradusit/test5.java
@@ -0,0 +1,17 @@
+package com.gradusit;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Gustavo
+ * Date: 10/10/14
+ * Time: 10:51
+ * To change this template use File | Settings | File Templates.
+ */
+public class test5 {
+    public static void main(String args[]){
+        int count;
+        for(count = 0; count < 10; count++ ) {
+            System.out.println("Valor del contador: " + count);
+        }
+    }
+}
