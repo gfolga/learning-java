b3e3b56ce4263eac3258144ad1fafe602e3d9fa3
Gustavo Folga <gfolga@salt.com>
2014-10-12 12:54:55 -0200

    arrays2

---
+++ src/com/gradusit/array/array2.java
@@ -0,0 +1,18 @@
+package com.gradusit.array;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Gustavo
+ * Date: 12/10/14
+ * Time: 12:45
+ * To change this template use File | Settings | File Templates.
+ */
+public class array2 {
+    public static void main(String args[]){
+        int misnumeros[] = { 1, 2, 54, 6 , 6,99, 11 };
+
+        System.out.println("misnumeros[0] es " + misnumeros[0]);
+        System.out.println("misnumeros[2] es " + misnumeros[2]);
+        System.out.println("misnumeros[1] es " + misnumeros[1]);
+    }
+}
