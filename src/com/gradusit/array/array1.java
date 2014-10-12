515e6f849f6e7bf7079f0f0dc3d21321cee8350e
Gustavo Folga <gfolga@salt.com>
2014-10-12 12:51:40 -0200

    arrays

---
+++ src/com/gradusit/array/array1.java
@@ -0,0 +1,22 @@
+package com.gradusit.array;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Gustavo
+ * Date: 12/10/14
+ * Time: 12:45
+ * To change this template use File | Settings | File Templates.
+ */
+public class array1 {
+    public static void main(String args[]){
+        int misnumeros[];
+        misnumeros = new int[10];
+        misnumeros[0] = 1;
+        misnumeros[1] = 199;
+        misnumeros[2] = 12;
+        misnumeros[3] = 11;
+        System.out.println("misnumeros[0] es " + misnumeros[0]);
+        System.out.println("misnumeros[2] es " + misnumeros[2]);
+        System.out.println("misnumeros[1] es " + misnumeros[1]);
+    }
+}
